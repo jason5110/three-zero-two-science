eb54e07df65c64fdc562f292e69aabc3dd1bed1e
Jason Huang <jason_huang@Jasons-MacBook-Pro.local>
2025-02-18 15:13:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React + TypeScript + Vite
+
+- create project with vite
+
+- this project is [viewsonic] FE 作業考題
+
+
+請用 Vite + React + Styled-Components  + Redux 實做出以下範例
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { AliasOptions, defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc'
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    } as AliasOptions,
+  },
+  server: {
+    port: 3000,
+  }
+})
